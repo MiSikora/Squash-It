5394e64370c80b5cd3777aee9f6ef518054a840b
Michał Sikora <me@mehow.io>
2020-01-10 10:50:23 +0100

    Allow to display prompt messages.

--- squash-it/app/src/main/java/io/mehow/squashit/MainActivity.kt
+++ squash-it/app/src/main/java/io/mehow/squashit/MainActivity.kt
@@ -2,14 +2,34 @@ package io.mehow.squashit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.BaseTransientBottomBar.Duration
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
+import com.google.android.material.snackbar.Snackbar
 import dagger.android.AndroidInjection
 import io.mehow.squashit.databinding.MainBinding
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding: MainBinding
+  private var currentSnackbar: Snackbar? = null
+
   override fun onCreate(inState: Bundle?) {
     AndroidInjection.inject(this)
     super.onCreate(inState)
-    val binding = MainBinding.inflate(layoutInflater)
+    binding = MainBinding.inflate(layoutInflater)
     setContentView(binding.root)
   }
+
+  fun showPrompt(
+    message: String,
+    @Duration duration: Int = LENGTH_INDEFINITE,
+    builder: Snackbar.() -> Unit = {}
+  ) {
+    hidePrompt()
+    currentSnackbar = Snackbar.make(binding.root, message, duration).also { snackbar ->
+      snackbar.builder()
+      snackbar.show()
+    }
+  }
+
+  fun hidePrompt() = currentSnackbar?.dismiss()
 }
