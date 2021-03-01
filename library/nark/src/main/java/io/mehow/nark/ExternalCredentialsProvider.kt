59057a3c466719f142927a2ea4347da7bb501de5
Michał Sikora <michalsikora90@gmail.com>
2021-03-01 19:01:24 +0100

    Add credentials provider for complementary app

---
+++ library/nark/src/main/java/io/mehow/nark/ExternalCredentialsProvider.kt
@@ -0,0 +1,23 @@
+package io.mehow.nark
+
+import android.content.Context
+import androidx.core.net.toUri
+
+internal class ExternalCredentialsProvider(
+  private val context: Context,
+  userId: String,
+) : CredentialsProvider {
+  private val uri = "content://io.mehow.nark.contentprovider/credentials/$userId".toUri()
+
+  override fun provide() = context.contentResolver.query(uri, null, null, null, null)?.use { cursor ->
+    if (!cursor.moveToFirst()) return@use null
+
+    val idIndex = cursor.getColumnIndex("id")
+    val id = cursor.getString(idIndex)
+
+    val secretIndex = cursor.getColumnIndex("secret")
+    val secret = cursor.getString(secretIndex)
+
+    Credentials(id, secret)
+  }
+}
